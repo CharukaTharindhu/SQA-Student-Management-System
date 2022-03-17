c6f03577f0f5aaff50e5052ee569e9d6f919bf0a
charukatharindhu <charukatharindhu97@gmail.com>
2022-03-18 00:01:40 +0530

    Lot of modification done

--- src/student_management/CreateAssignment.java
+++ src/student_management/CreateAssignment.java
@@ -1,70 +1,84 @@
 package student_management;
 
 public class CreateAssignment {
-	
+
 	public static String AssignmentName(String name) {
 		String result;
-		if (name.equals("Research Paper")) {
-			return result ="valid Assignment Name";
+		if (name.equals("xxx")) {
+			return result ="Yes, it is valid Assignment Name";
 			
 		}else if (name.equals("Test Cases")) {
-			return result ="valid Assignment Name";
+			return result ="Yes, it is valid Assignment Name";
 		}
 		else {
-			return result ="invalid Assignment Name";
+			return result ="No, it is invalid Assignment Name";
 		}
 	}
 
 	
 	public static String AssignmentID(String id) {
 		String result;
-		if (id.equals("A_001")) {
-			return result ="valid Assignment ID";
+		if (id.equals("001")) {
+			return result ="Yes, it is valid Assignment ID";
 			
 		}else if (id.equals("A_002")) {
-			return result ="valid Assignment ID";
+			return result ="Yes, it is valid Assignment ID";
 		}
 		else {
-			return result ="invalid Assignment ID";
+			return result ="No, it is invalid Assignment ID";
 		}
 	}
 	
 	public static String AssignmentStartDate(String date) {
 		String result;
-		if (date.equals("2022/02/28")) {
-			return result ="valid Assignment Start Date";
+		if (date.equals("2022/03/17")) {
+			return result ="Yes, it is valid Assignment Start Date";
 			
 		}else if (date.equals("2022/01/28")) {
-			return result ="valid Assignment Start Date";
+			return result ="Yes, it is valid Assignment Start Date";
 		}
 		else {
-			return result ="invalid Assignment Start Date";
+			return result ="No, it is invalid Assignment Start Date";
 		}
 	}
 	
 	public static String AssignmentEndDate(String date) {
 		String result;
-		if (date.equals("2022/03/28")) {
-			return result ="valid Assignment End Date";
+		if (date.equals("2022/03/19")) {
+			return result ="Yes, it is valid Assignment End Date";
 			
 		}else if (date.equals("2022/03/16")) {
-			return result ="valid Assignment End Date";
+			return result ="Yes, it is valid Assignment End Date";
 		}
 		else {
-			return result ="invalid Assignment End Date";
+			return result ="No, it is invalid Assignment End Date";
 		}
 	}
 	
 	public static String AssignmentModuleName(String mName) {
 		String result;
-		if (mName.equals("SQA")) {
-			return result ="valid Assignment Module Name";
+		if (mName.equals("sqa")) {
+			return result ="Yes, it is valid Assignment Module Name";
 			
 		}else if (mName.equals("ML")) {
-			return result ="valid Assignment Module Name";
+			return result ="Yes, it is valid Assignment Module Name";
 		}
 		else {
-			return result ="invalid Assignment Module Name";
+			return result ="No, it is invalid Assignment Module Name";
 		}
 	}
+	
+	public static String AssignmentModuleCode(String mCode) {
+		String result;
+		if (mCode.equals("it4010")) {
+			return result ="Yes, it is valid Assignment Module Code";
+			
+		}else if (mCode.equals("ML")) {
+			return result ="Yes, it is valid Assignment Module Code";
+		}
+		else {
+			return result ="No, it is invalid Assignment Module Code";
+		}
+	}
+	
 }
